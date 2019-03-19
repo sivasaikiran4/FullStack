5af5d5b2ebb54ed6381cf26033b8a245db3bf6b2
yoga-user <yoga-user@yogauser-Lenovo-YOGA-720-15IKB>
2019-03-19 12:59:04 -0500

    @ModelAttribute

---
+++ MVCSetup/src/org/practice/spring/HelloController.java
@@ -0,0 +1,25 @@
+package org.practice.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public ModelAndView helloWorld(@ModelAttribute("info") Information userInfo){
+		 
+		ModelAndView model = new ModelAndView("hello"); 
+		model.addObject("firstName",userInfo.getFirstName());
+		model.addObject("lastName",userInfo.getLastName());
+		return model;
+	}
+	
+	@RequestMapping("/")
+	public ModelAndView homepage(){
+    ModelAndView model = new ModelAndView("index","info", new Information());   
+	return model;
+	}
+}
